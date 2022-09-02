72a3afd478705aac8625399029e3bd7c2997d5e7
VICTORIA AMORIM NOVAES DE SOUSA <victoria.ansousa@senacsp.edu.br>
2022-09-02 11:52:22 -0300

    Atualização com o botão

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,34 @@
 const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click',(evento) => {
-evento.preventDefault()
-const input = document.querySelector('[data-form-input]')
-const lista = document.querySelector('[data-list]')
-const valor = input.value
-const tarefa = document.createElement('li')
-tarefa.classList.add('task')
-const conteudo = `<p class="constant">${valor}</p>`
-tarefa.innerHTML = conteudo
-lista.appendChild(tarefa)
+    evento.preventDefault()
+    const input = document.querySelector('[data-form-input]')
+    const lista = document.querySelector('[data-list]')
+    const valor = input.value
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="constant">${valor}</p>`
+    tarefa.innerHTML = conteudo
 
-input.value = ""
+    tarefa.appendChild(botao())
 
+
+    lista.appendChild(tarefa)
+
+    input.value = ""
 
 })
+
+const botao = () =>{
+
+    const botaoConclui = document.createElement('button')
+    botaoConclui.innerHTML = 'conclui'
+    botaoConclui.addEventListener('click',() =>{
+        console.log("Fui clicado")
+    })
+
+    return botaoConclui
+
+}
 
 
